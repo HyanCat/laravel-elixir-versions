21accd1396d18a21563af245678a217a26423118
HyanCat <hyancat@live.cn>
2015-05-14 23:59:46 +0800

    maybe ok.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # laravel-elixir-versions
-laravel-elixir-versions
+
+## Description
+
+This is the copy of `elixir-version`, but it supports multiple building version numbers.
+
+You konw, In `laravel-elixir`, the `version` extension could be run only once.
+
+But sometimes you have to run `mix.version` more than once.
+So, it will.
+
+
+## usage
+
+```javascript
+
+elixir(function (mix) {
+
+	// some code before....
+	
+	mix.versions([xxx], 'path_xx');
+
+	mix.versions([yyy], 'path_yy');
+	
+	// some code after.... //
+});
+
+```
+
---
+++ index.js
@@ -0,0 +1,127 @@
+var utilities  = require('./utilities');
+var parsePath  = require('parse-filepath');
+var vinylPaths = require('vinyl-paths');
+var elixir     = require('laravel-elixir');
+var rev        = require('gulp-rev');
+var gulp       = require('gulp');
+var del        = require('del');
+var fs         = require('fs');
+
+var publicDir = elixir.config.publicDir;
+
+/*
+ |----------------------------------------------------------------
+ | Versioning / Cache Busting
+ |----------------------------------------------------------------
+ |
+ | This task will append a small hash on the end of your file
+ | and generate a manifest file which contains the current
+ | "version" of the filename for the application to use.
+ |
+ */
+
+elixir.extend('versions', function (src, buildDir) {
+	src = utilities.prefixDirToFiles(publicDir, src);
+	if (!elixir.config.versions) {
+		elixir.config.versions = new Array();
+	}
+	elixir.config.versions.push({
+		src     : src,
+		buildDir: buildDir
+	});
+	buildTask(src, buildDir);
+
+	this.registerWatcher('versions', src);
+
+	return this.queueTask('versions');
+});
+
+
+/**
+ * Build the "version" Gulp task.
+ *
+ * @param  {string} src
+ * @param  {string} buildDir
+ * @return {object}
+ */
+var buildTask = function (src, buildDir) {
+
+	gulp.task('versions', function () {
+
+		elixir.config.versions.map(function (version) {
+			console.log('-----' + version.src + '-----');
+			src      = version.src;
+			buildDir = buildDir = getBuildDir(version.buildDir);
+
+			var files = vinylPaths();
+
+			utilities.logTask("Versioning", src);
+
+			// To start, we'll clear out the build directory,
+			// so that we can start from scratch.
+			del.sync(buildDir + '/*', {force: true});
+
+			return gulp.src(src, {base: './' + publicDir})
+				.pipe(gulp.dest(buildDir))
+				.pipe(files)
+				.pipe(rev())
+				.pipe(gulp.dest(buildDir))
+				.pipe(rev.manifest())
+				.pipe(gulp.dest(buildDir))
+				.on('end', function () {
+					// We'll get rid of the duplicated file that
+					// usually gets put in the "build" folder,
+					// alongside the suffixed version.
+					del(files.paths, {force: true});
+
+					// We'll also copy over relevant sourcemap files.
+					copyMaps(src, buildDir);
+				});
+		});
+	});
+
+};
+
+
+/**
+ * Prepare the path to the build directory.
+ *
+ * @param  {string} buildDir
+ * @return {string}
+ */
+var getBuildDir = function (buildDir) {
+	var baseDir = buildDir || publicDir;
+
+	return baseDir + '/build';
+};
+
+
+/**
+ * Copy source maps to the build directory.
+ *
+ * @param  {string} src
+ * @param  {string} buildDir
+ * @return {object}
+ */
+var copyMaps = function (src, buildDir) {
+
+	// We'll first get any files from the src
+	// array that have companion .map files.
+	var mappings = [];
+
+	src.forEach(function (file) {
+		var map = file + '.map';
+
+		if (fs.existsSync(map)) {
+			mappings.push(map);
+		}
+	});
+
+	// And then we'll loop over this mapping array
+	// and copy each over to the build directory.
+	mappings.forEach(function (mapping) {
+		var map = mapping.replace(publicDir, buildDir);
+
+		gulp.src(mapping).pipe(gulp.dest(parsePath(map).dirname));
+	});
+};
